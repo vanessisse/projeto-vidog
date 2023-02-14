ded490f425e9e0c786469a72b42b7207d67cd3d6
vanessisse <vanessasillos@gmail.com>
2023-02-14 16:38:49 -0300

    Criei este projeto durante as aulas do Módulo 1 - Modern Web do curso de Desenvolvimento Full Stack da iTalents com a orientação da professora Maria Eduarda Cardoso. Também fiz o design da logo que se encontra no cabeçalho.

--- js/favorito.js
+++
@@ -1,9 +0,0 @@
-//  Estilização do coração
-
-let coracoes = document.querySelectorAll('.bi-heart-fill');
-
-coracoes.forEach(function(coracao){
-    coracao.addEventListener('click', function(event){
-        event.target.classList.toggle('coracao');
-    });
-});
